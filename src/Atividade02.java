be8b1c65e150a03fb423d64afbb17478ba3d4eea
maycow.carmo <maycow.carmo@gmail.com>
2017-03-27 22:32:39 -0300

    Atividade quase ok

--- src/Atividade02.java
+++ src/Atividade02.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by maiki on 24/03/2017.
@@ -7,38 +10,131 @@ import java.awt.image.BufferedImage;
 public class Atividade02 {
 
 
+    static float contraste[][] = {
+            { 0, -1,  0},
+            {-1,  5, -1},
+            { 0, -1,  0}
+    };
+
+    public static int saturate(int value){
+        if (value > 255) {
+            return 255;
+        }
+        if(value < 0){
+            return 0;
+        }
+        return value;
+    }
+
+    static Color applyKernel(Color[][] colors, float[][] kernel)
+    {
+        Color newColors[][] = new Color[3][3];
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                int r = saturate((int) (colors[x][y].getRed() * kernel[x][y]));
+                int g = saturate((int) (colors[x][y].getGreen() * kernel[x][y]));
+                int b = saturate((int) (colors[x][y].getBlue() * kernel[x][y]));
+                newColors[x][y] = new Color(r, g, b);
+            }
+
+        }
+        int sr = 0;
+        int sg = 0;
+        int sb = 0;
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                sr +=  newColors[x][y].getRed();
+                sg +=  newColors[x][y].getGreen();
+                sb +=  newColors[x][y].getBlue();
+            }
+        }
+        return new Color(sr, sg, sb);
+
+
+    }
+
+    static Color getColor (BufferedImage img, int x, int y)
+    {
+       //BufferedImage pixelImg = pixelate(img,10);
+        if(x < 0 || x >= img.getWidth() || y < 0 || y >= img.getHeight())
+            return new Color(0,0,0);
+
+        return new Color (img.getRGB(x, y));
+
+    }
+
+    static Color[][] getPixelColor(BufferedImage img, int x, int y)
+    {
+        return new Color[][]{
+                {getColor(img,x-1, y-1), getColor(img, x, y-1), getColor(img, x+1, y-1)},
+                {getColor(img,x-1, y+0), getColor(img, x, y+0), getColor(img, x+1, y+0)},
+                {getColor(img,x-1, y+1), getColor(img, x, y+1), getColor(img, x+1, y+1)}
+
+        };
+
+    }
+
+
+    static BufferedImage convolve(BufferedImage img, float[][] kernel, int pixelSize)
+    {
+        BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < img.getHeight(); y+=pixelSize) {
+            for (int x = 0; x < img.getWidth() ; x+=pixelSize) {
+                Color colors = new Color(applyKernel(getPixelColor(img, x, y), kernel).getRGB());
+
+                out.setRGB(x, y, colors.getRGB());
+            }
+
+        }
+        return out;
+    }
+
+
     public static BufferedImage pixelate(BufferedImage img, int pixelSize)
     {
+
         BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(),img.getType());
 
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
-                Color pixelColor = new Color(img.getRGB(x,y));
+        for (int y = 0; y < img.getHeight();  y += pixelSize) {
+            for (int x = 0; x < img.getWidth(); x += pixelSize) {
 
-                Graphics pixelRect = img.getGraphics();
-                pixelRect.setColor(pixelColor);
-                pixelRect.fillRect(x,y,pixelSize, pixelSize);
-
-                int r = pixelColor.getRed();
-                int g = pixelColor.getGreen();
-                int b = pixelColor.getBlue();
-
-                Color outColor = new Color(r,g,b);
-                out.setRGB(x,y,outColor.getRGB());
+                setProxCor(img, out, x, y, pixelSize);
             }
         }
 
         return out;
     }
 
-    public static void run()
+    public static void setProxCor(BufferedImage img,BufferedImage out, int x, int y, int pixelSize)
     {
+        Color newColor = new Color(img.getRGB(x,y));
 
+        for (int h = y; h < y + pixelSize; h++) {
+            for (int w = x; w < x + pixelSize; w++) {
+
+                if(h >= img.getHeight() || w >= img.getWidth())
+                {
+                    continue;
+                }
+                out.setRGB(w, h, newColor.getRGB());
+            }
+        }
+    }
+
+    public static void run() throws IOException {
+        String PATH = "C:\\Users\\maycow.carmo\\img\\cor";
+        BufferedImage img = ImageIO.read(new File(PATH,"metroid1.jpg"));
+        BufferedImage pixelateImg = pixelate(img, 10);
+        BufferedImage conv = convolve(pixelateImg, contraste, 10);
+
+
+        ImageIO.write(pixelateImg, "png", new File("pixelPuppy.png"));
+        ImageIO.write(conv, "png", new File("conv.png"));
 
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         new Atividade02().run();
     }
 }
