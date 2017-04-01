3c141603871dc962ba73c5254e7a53767f6f88c3
maroeca <maycow.carmo@gmail.com>
2017-04-01 16:59:07 -0300

    ATIVIDADE 1 "CERTA"

--- src/Atividade02.java
+++ src/Atividade02.java
@@ -10,13 +10,13 @@ import java.io.IOException;
 public class Atividade02 {
 
 
-    static float contraste[][] = {
+    static float contraste[][] = { //kernel de contaste
             { 0, -1,  0},
             {-1,  5, -1},
             { 0, -1,  0}
     };
 
-    public static int saturate(int value){
+    public static int saturate(int value){ //pra não estourar a cor
         if (value > 255) {
             return 255;
         }
@@ -26,11 +26,11 @@ public class Atividade02 {
         return value;
     }
 
-    static Color applyKernel(Color[][] colors, float[][] kernel)
+    static Color applyKernel(Color[][] colors, float[][] kernel) //aplica o kernel
     {
         Color newColors[][] = new Color[3][3];
         for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
+            for (int y = 0; y < 3; y++) { //lê os pixels e multiplica pelo kernel
                 int r = saturate((int) (colors[x][y].getRed() * kernel[x][y]));
                 int g = saturate((int) (colors[x][y].getGreen() * kernel[x][y]));
                 int b = saturate((int) (colors[x][y].getBlue() * kernel[x][y]));
@@ -42,7 +42,7 @@ public class Atividade02 {
         int sg = 0;
         int sb = 0;
         for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
+            for (int y = 0; y < 3; y++) { //soma tudo nas variasveis pra retornar
                 sr +=  newColors[x][y].getRed();
                 sg +=  newColors[x][y].getGreen();
                 sb +=  newColors[x][y].getBlue();
@@ -55,7 +55,7 @@ public class Atividade02 {
 
     static Color getColor (BufferedImage img, int x, int y)
     {
-       //BufferedImage pixelImg = pixelate(img,10);
+       //não deixa passar os limites das bordas
         if(x < 0 || x >= img.getWidth() || y < 0 || y >= img.getHeight())
             return new Color(0,0,0);
 
@@ -63,7 +63,7 @@ public class Atividade02 {
 
     }
 
-    static Color[][] getPixelColor(BufferedImage img, int x, int y)
+    static Color[][] getPixelColor(BufferedImage img, int x, int y) //lê os pixels vizinhos
     {
         return new Color[][]{
                 {getColor(img,x-1, y-1), getColor(img, x, y-1), getColor(img, x+1, y-1)},
@@ -75,14 +75,14 @@ public class Atividade02 {
     }
 
 
-    static BufferedImage convolve(BufferedImage img, float[][] kernel, int pixelSize)
+    static BufferedImage convolve(BufferedImage img, float[][] kernel, int pixelSize) //aplica o filtro na imagem
     {
         BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 
         for (int y = 0; y < img.getHeight(); y+=pixelSize) {
             for (int x = 0; x < img.getWidth() ; x+=pixelSize) {
                 Color colors = new Color(applyKernel(getPixelColor(img, x, y), kernel).getRGB());
-
+                setProxCorContraste(img,out,x,y,pixelSize,colors);
                 out.setRGB(x, y, colors.getRGB());
             }
 
@@ -106,8 +106,21 @@ public class Atividade02 {
         return out;
     }
 
+    public static void setProxCorContraste(BufferedImage img, BufferedImage out, int x, int y, int pixelSize, Color cor)
+    { //aplica o contraste pro tamanho certo dos pixels
+        for (int h = y; h < y + pixelSize ; h++) {
+            for (int w = x; w < x + pixelSize ; w++) {
+                if(h >= img.getHeight() || w >= img.getWidth())
+                {
+                    continue;
+                }
+                out.setRGB(w,h, cor.getRGB());
+            }
+        }
+    }
+
     public static void setProxCor(BufferedImage img,BufferedImage out, int x, int y, int pixelSize)
-    {
+    {//seta a cor por tamanho dos pixels da função pixelate
         Color newColor = new Color(img.getRGB(x,y));
 
         for (int h = y; h < y + pixelSize; h++) {
@@ -123,14 +136,14 @@ public class Atividade02 {
     }
 
     public static void run() throws IOException {
-        String PATH = "C:\\Users\\maycow.carmo\\img\\cor";
+        String PATH = "C:\\Users\\maiki\\Pictures\\img\\cor";
         BufferedImage img = ImageIO.read(new File(PATH,"metroid1.jpg"));
-        BufferedImage pixelateImg = pixelate(img, 10);
-        BufferedImage conv = convolve(pixelateImg, contraste, 10);
+        BufferedImage pixelateImg = pixelate(img, 15); //faz o pixel
+        BufferedImage pixelTraste = convolve(pixelateImg, contraste, 15); // aplica o contraste
 
 
         ImageIO.write(pixelateImg, "png", new File("pixelPuppy.png"));
-        ImageIO.write(conv, "png", new File("conv.png"));
+        ImageIO.write(pixelTraste, "png", new File("conv.png"));
 
     }
 
